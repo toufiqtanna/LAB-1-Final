921cbdd143099104acb96035348f59d66b55e85c
toufiqtanna <toufiqtanna@gmail.com>
2020-11-23 20:17:57 +0600

    first commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    abstract class Account
+    {
+        private string accName;
+        private string accID;
+        protected double balance;
+
+        public string AccName
+        {
+            get {return accName;}
+            set { value = accName;}
+        }
+        public string AccID
+        {
+             get { return accID; }
+             set { value = accID; }
+        }
+        public double Balance
+        {
+              get { return balance; }
+              set { value = balance; }
+        }
+        public Account() { }
+        public Account(string accName, string accID, double balance)
+        {
+            this.accName = accName;
+            this.accID = accID;
+            this.balance = balance;
+        }
+        public abstract void Deposit(double amount);
+        public abstract void Withdraw(double amount);
+        public abstract void Transactions(double amount, string accID);
+
+        virtual public void ShowInfo()
+        {
+            Console.WriteLine("Name: " +accName);
+            Console.WriteLine("AccNo: " +accID);
+            Console.WriteLine("Balance: " +balance);
+        }
+    }
+}
---
+++ BankApp/SavingsAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class SavingsAccount: Account
+    {
+        public List<Transaction> Transactions { get; set; }
+        public override void Deposit(double amount)
+        {
+            balance = +amount;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if(amount < balance)
+            {
+                if (balance > 500)
+                {
+                    balance -= amount;
+                }
+                else
+                    Console.WriteLine("Blance is less than 500tk");
+            }
+            else
+                Console.WriteLine("Insufficient balance");
+        }
+        public override void Transactions(double amount, string accID)
+        {
+            balance += amount;
+
+
+        }
+
+    }
+}
